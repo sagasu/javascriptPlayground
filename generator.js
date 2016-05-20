34067678c8ca056bf91640582ef196af50095e2c
Mateusz Kopij <sagasu@gmail.com>
2016-05-20 14:54:23 +0100

    playing with generators

---
+++ generator.js
@@ -0,0 +1,18 @@
+var fibonacci2 = {
+    [Symbol.iterator]: function*() {
+        var pre = 0, cur = 1;
+        for (;;) {
+            var temp = pre;
+            pre = cur;
+            cur += temp;
+            yield cur;
+        }
+    }
+}
+
+for (var n of fibonacci2) {
+    // truncate the sequence at 1000
+    if (n > 1000)
+        break;
+    console.log(n);
+}
