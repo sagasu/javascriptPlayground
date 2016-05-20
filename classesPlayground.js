32d29886e58e9dad0acaef0292da74594e5c4b2f
Mateusz Kopij <sagasu@gmail.com>
2016-05-20 11:42:22 +0100

    adding some logging to class to check if execution order is expected.

--- classesPlayground.js
+++ classesPlayground.js
@@ -1,12 +1,16 @@
 // Vehicle needs to be defined before Car in file :) otherwise chrome throws exception
 class Vehicle{
     update(price) {
-
+        console.log('vehicle update has been called')
     }
 }
 
 class Car extends Vehicle {
+    // it will be executed as C# constructor
     constructor(color, price) {
+        console.log('constructor method has been called')
+
+        //interesting that Vehicle class doesn't has a constructor, but no exception is thrown.
         super(color, price);
 
         this.car = Car.defaultVehicle();
@@ -17,10 +21,12 @@ class Car extends Vehicle {
 
     update(price) {
         //...
+        console.log('update has been called')
         super.update(price);
     }
 
     static defaultVehicle() {
+        console.log('static method has been called')
         return new Vehicle();
     }
 }
