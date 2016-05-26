af3a49124169dd44ffc5d72f0f8185e9a86d2607
Mateusz Kopij <sagasu@gmail.com>
2016-05-26 08:58:23 +0100

    differences between let and var checking how it really works.

--- letConst.js
+++ letConst.js
@@ -17,3 +17,36 @@ function f() {
 }
 
 var z = f();
+
+
+// no difference between let and var in global scope
+let me = 'go'; //globally scoped
+var i = 'able'; //globally scoped
+
+// no difference between let and var in function scope
+function ingWithinEstablishedParameters() {
+    let terOfRecommendation = 'awesome worker!'; //function block scoped
+    var sityCheerleading = 'go!'; //function block scoped
+};
+
+// var scope is function scope based, while let is block scope based
+function allyIlliterate() {
+    //tuce is *not* visible out here
+
+    for( let tuce = 0; tuce < 5; tuce++ ) {
+        //tuce is only visible in here (and in the for() parentheses)
+    };
+
+    //tuce is *not* visible out here
+};
+
+// var scope is function scope based, while let is block scope based
+function byE40() {
+    //nish *is* visible out here
+
+    for( var nish = 0; nish < 5; nish++ ) {
+        //nish is visible to the whole function
+    };
+
+    //nish *is* visible out here
+};
